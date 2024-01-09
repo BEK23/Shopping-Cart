a212a706ac9a99c2a0e4f4e2785a26940a12a3cd
_BEK <62075760+BEK23@users.noreply.github.com>
2024-01-09 17:13:26 +0500

    config: eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import "./index.css";
+
+import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { NextUIProvider } from "@nextui-org/react";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
