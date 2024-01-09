87e59dde98aabbbc492719f7cb7db34818d10865
_BEK <62075760+BEK23@users.noreply.github.com>
2024-01-09 17:06:08 +0500

    add: tailwind, nextui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <NextUIProvider>
+      <div></div>
+    </NextUIProvider>
+  </React.StrictMode>,
 );
